540da69af18156e4702fa83825b9bef501ec2380
Charlie Root <root@joereg4.com>
2023-05-07 01:58:46 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import feedparser
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Parse the RSS feed
+    feed = feedparser.parse('http://jregenstein.com/feed')
+
+    # Build the HTML response
+    html = '<h1>{}</h1>'.format(feed.feed.title)
+    html += '<ul>'
+    for entry in feed.entries:
+        html += '<li><a href="{}">{}</a></li>'.format(entry.link, entry.title)
+    html += '</ul>'
+
+    return html
+
+if __name__ == '__main__':
+    app.run()
