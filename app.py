1f47979ea5ea1b011fc56f17a0ec7022167b1891
joereg4 <59125710+joereg4@users.noreply.github.com>
2023-05-07 09:02:23 -0500

    added pagination

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+""" from flask import Flask
 import feedparser
 
 app = Flask(__name__)
@@ -12,6 +12,39 @@ def index():
     html = '<h1>{}</h1>'.format(feed.feed.title)
     html += '<ul>'
     for entry in feed.entries:
+        html += '<li><a href="{}">{}</a></li>'.format(entry.link, entry.title)
+    html += '</ul>'
+
+    return html
+
+if __name__ == '__main__':
+    app.run() """
+
+from flask import Flask
+import feedparser
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    base_url = 'http://jregenstein.com/feed'
+    page = 1
+    max_pages = 5  # Set the maximum number of pages you want to fetch
+    entries = []
+
+    while page <= max_pages:
+        feed = feedparser.parse(f'{base_url}?page={page}')
+
+        if not feed.entries:
+            break
+
+        entries.extend(feed.entries)
+        page += 1
+
+    # Build the HTML response
+    html = '<h1>{}</h1>'.format(feed.feed.title)
+    html += '<ul>'
+    for entry in entries:
         html += '<li><a href="{}">{}</a></li>'.format(entry.link, entry.title)
     html += '</ul>'
 
